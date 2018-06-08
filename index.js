b6bd264c750d8d0b948b89f0052083abdb70d399
shay-kennedy <kennedy.rshay@gmail.com>
2018-06-08 00:48:25 -0700

    updating links

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 $(document).ready(() => {
 
 	$('.me').mouseenter(() => {
-		$('.me').attr('src', './images/profile-pic-2.jpg');
+		$('.me').attr('src', './images/shay-kennedy-2.jpg');
 	})
 	.mouseout(() => {
-		$('.me').attr('src', './images/profile-pic-1.jpg');
+		$('.me').attr('src', './images/shay-kennedy-1.jpg');
 	})
 
 });
